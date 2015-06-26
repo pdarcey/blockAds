1abaf1db30eac4e6bf0c02de4b694a237231a311
Paul Darcey <pdarcey@gmail.com>
2015-06-26 15:51:49 +1000

    Updated json file to include mimic blockAd.css

---
+++ contentBlocker/ActionRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  ActionRequestHandler.h
+//  contentBlocker
+//
+//  Created by Paul Darcey on 26/06/2015.
+//  Copyright © 2015 Paul Darcey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionRequestHandler : NSObject <NSExtensionRequestHandling>
+
+@end
---
+++ contentBlocker/ActionRequestHandler.m
@@ -0,0 +1,26 @@
+//
+//  ActionRequestHandler.m
+//  contentBlocker
+//
+//  Created by Paul Darcey on 26/06/2015.
+//  Copyright © 2015 Paul Darcey. All rights reserved.
+//
+
+#import "ActionRequestHandler.h"
+
+@interface ActionRequestHandler ()
+
+@end
+
+@implementation ActionRequestHandler
+
+- (void)beginRequestWithExtensionContext:(NSExtensionContext *)context {
+    NSItemProvider *attachment = [[NSItemProvider alloc] initWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"blockerList" withExtension:@"json"]];
+    
+    NSExtensionItem *item = [[NSExtensionItem alloc] init];
+    item.attachments = @[attachment];
+    
+    [context completeRequestReturningItems:@[item] completionHandler:nil];
+}
+
+@end
